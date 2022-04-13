1333b04366d5357f29bc6aadca3a7f5ad2f39a32
dungonfq <57652048+dungonfq@users.noreply.github.com>
2022-04-13 23:29:21 +0700

    Twin.macro + @emotion

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,13 +1,22 @@
+import tw from 'twin.macro'
 import Header from '@/components/core/Header'
 import Navbar from '@/components/core/Navbar'
 import Footer from '@/components/core/Footer'
 
-const LayoutDefault = ({ children }) => {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const styles = {
+  container: tw`container mx-auto`,
+}
+
+const LayoutDefault = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
       <Navbar />
-      <div id="container" className="container">
+      <div id="container" css={styles.container}>
         {children}
       </div>
       <Footer />
